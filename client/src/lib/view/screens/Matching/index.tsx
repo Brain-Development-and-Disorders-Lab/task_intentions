e760743691e771c48f9681c5fb6833ecb284b82a
Henry Burgess <henryjburg@gmail.com>
2024-11-26 13:24:52 -0600

    Update `Matching` screen

--- client/src/lib/view/screens/Matching/index.tsx
+++ client/src/lib/view/screens/Matching/index.tsx
@@ -8,7 +8,7 @@
  */
 
 // React import
-import React, { FC, ReactElement } from "react";
+import React, { FC, ReactElement, useEffect } from "react";
 
 // Logging library
 import consola from "consola";
@@ -18,9 +18,6 @@ import { Box, Heading, Layer, Spinner, WorldMap } from "grommet";
 
 // Request library
 import Compute from "src/lib/classes/Compute";
-
-// Configuration
-import { Configuration } from "src/configuration";
 
 /**
  * @summary Generate a 'Matching' screen presenting a loading indicator and
@@ -34,103 +31,87 @@ const Matching: FC<Props.Screens.Matching> = (
 ): ReactElement => {
   const experiment = window.Experiment;
 
-  // Launch request
-  if (props.fetchData) {
-    // Setup a new 'Compute' instance with the configured endpoint URL
-    const compute = new Compute(Configuration.endpoint);
+  const callback = (data: ModelResponse) => {
+    // Parse and store the JSON content
+    try {
+      // Extract the response data of interest
+      // Participant data
+      const participantParameters = data.participantParameters;
 
-    // Collate data from 'playerChoice' trials
-    consola.info(`Collating data...`);
-    const dataCollection = jsPsych.data
-      .get()
-      .filter({
-        display: "playerChoice",
-      })
-      .values();
+      // Partner data
+      const partnerParameters = data.partnerParameters;
+      const partnerChoices = data.partnerChoices;
 
-    consola.debug(
-      `'dataCollection' containing trials with 'display' = 'playerChoice':`,
-      dataCollection
-    );
+      // Check the specification of the data first, require exactly 54 trials
+      if (partnerChoices.length > 0) {
+        // Store the partner choices
+        experiment.getState().set("partnerChoices", partnerChoices);
 
-    // Format the responses to be sent to the server
-    const requestResponses = [];
-    for (const row of dataCollection) {
-      requestResponses.push({
-        ID: "NA",
-        Trial: row.trial,
-        ppt1: row.playerPoints_option1,
-        par1: row.partnerPoints_option1,
-        ppt2: row.playerPoints_option2,
-        par2: row.partnerPoints_option2,
-        Ac: row.selectedOption_player,
-        Phase: 1,
-      });
-    }
-    consola.debug(`Request content 'requestResponses':`, requestResponses);
+        // Store parameters
+        props.handler(participantParameters, partnerParameters);
+      } else {
+        consola.warn(`Phase data appears to be incomplete`);
 
-    // Launch request to endpoint
-    consola.info(`Requesting partner...`);
-    compute.submit(
-      // Request data
-      {
-        participantID: experiment.getState().get("participantID"),
-        participantResponses: requestResponses,
-      },
-      // Response callback
-      (data: {
-        participantID: string;
-        participantParameters: number[];
-        partnerParameters: number[];
-        partnerChoices: {
-          ppt1: number;
-          par1: number;
-          ppt2: number;
-          par2: number;
-          Ac: number;
-        }[];
-      }) => {
-        // Parse and store the JSON content
-        try {
-          // Extract the response data of interest
-          // Participant data
-          const participantID = data.participantID;
-          const participantParameters = data.participantParameters;
-
-          // Partner data
-          const partnerParameters = data.partnerParameters;
-          const partnerChoices = data.partnerChoices;
-
-          // Check the specification of the data first, require exactly 54 trials
-          if (partnerChoices.length > 0) {
-            // Store the partner choices
-            experiment.getState().set("partnerChoices", partnerChoices);
-
-            consola.success(`Generated new partner for ID:`, participantID);
-
-            // Store parameters
-            props.handler(participantParameters, partnerParameters);
-          } else {
-            consola.warn(`Phase data appears to be incomplete`);
-
-            // If we have an error, we need to end the game
-            experiment.invokeError(
-              new Error("Incomplete response from server")
-            );
-          }
-        } catch (error) {
-          consola.warn(`Error occurred when extracting content:`, error);
-
-          // If we have an error, we need to end the game
-          experiment.invokeError(new Error("Error extracting content"));
-        }
-      },
-      (error) => {
         // If we have an error, we need to end the game
-        experiment.invokeError(error);
+        experiment.invokeError(
+          new Error("Incomplete response from server")
+        );
       }
-    );
-  }
+    } catch (error) {
+      consola.warn(`Error occurred when extracting content:`, error);
+
+      // If we have an error, we need to end the game
+      experiment.invokeError(new Error("Error extracting content"));
+    }
+  };
+
+  const runMatching = async () => {
+    // Launch request
+    if (props.fetchData) {
+      // Setup a new 'Compute' instance with the configured endpoint URL
+      const compute = new Compute();
+      await compute.setup();
+
+      // Collate data from 'playerChoice' trials
+      consola.info(`Collating data...`);
+      const dataCollection = jsPsych.data
+        .get()
+        .filter({
+          display: "playerChoice",
+        })
+        .values();
+
+      consola.debug(
+        `'dataCollection' containing trials with 'display' = 'playerChoice':`,
+        dataCollection
+      );
+
+      // Format the responses to be sent to the server
+      const requestResponses = [];
+      for (const row of dataCollection) {
+        requestResponses.push({
+          ID: "NA",
+          Trial: row.trial,
+          ppt1: row.playerPoints_option1,
+          par1: row.partnerPoints_option1,
+          ppt2: row.playerPoints_option2,
+          par2: row.partnerPoints_option2,
+          Ac: row.selectedOption_player,
+          Phase: 1,
+        });
+      }
+      consola.debug(`Request content 'requestResponses':`, requestResponses);
+
+      // Launch model computation
+      consola.info(`Requesting partner...`);
+      await compute.submit(requestResponses, callback);
+    }
+  };
+
+  // Run the matching process when first displayed
+  useEffect(() => {
+    runMatching();
+  });
 
   return (
     <>
