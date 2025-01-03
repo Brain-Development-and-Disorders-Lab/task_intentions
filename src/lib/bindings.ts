beac9d85e6f00c55750340e22c06bd90d05a3a59
Henry Burgess <henryjburg@gmail.com>
2025-01-03 16:07:01 -0600

    INT-43 Rename bindings

--- src/lib/bindings.ts
+++ src/lib/bindings.ts
@@ -4,8 +4,8 @@
 // specifically input from a button box and the MRI capture signal.
 
 export const BINDINGS = {
-  OPTION_ONE: "1", // Used to select the first option, or navigate previous
-  OPTION_TWO: "2", // Used to select the second option, or navigate next
+  PREVIOUS: "1", // Used to select the first option, or navigate previous
+  NEXT: "2", // Used to select the second option, or navigate next
   SELECT: "3", // Used to select an option
   SIGNAL: "5", // Received from MRI
 };
