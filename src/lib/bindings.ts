52bc1904e0dd8203774a9396c82d8182b25d7f6e
Henry Burgess <henryjburg@gmail.com>
2025-01-03 10:11:59 -0600

    INT-43 Add keyboard bindings variable

---
+++ src/lib/bindings.ts
@@ -0,0 +1,10 @@
+// bindings.ts
+// Author: Henry Burgess
+// Description: File contains the keybindings for the alternate input functionality,
+// specifically input from a button box and the MRI capture signal.
+
+export const BINDINGS = {
+  OPTION_ONE: "1", // Used to select the first option, or navigate previous
+  OPTION_TWO: "2", // Used to select the second option, or navigate next
+  SIGNAL: "5", // Received from MRI
+};
