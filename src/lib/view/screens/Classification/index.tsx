2ab605a8617ef7c7181d24a484558bfd25f47fed
Henry Burgess <henryjburg@gmail.com>
2025-01-06 18:57:40 -0600

    INT-43 Add `Keyboard` input to `Classification`

--- src/lib/view/screens/Classification/index.tsx
+++ src/lib/view/screens/Classification/index.tsx
@@ -8,11 +8,17 @@
  */
 
 // React import
-import React, { FC, ReactElement } from "react";
+import React, { FC, ReactElement, useState } from "react";
 
 // Grommet UI components
-import { Box, Button, Select, Paragraph } from "grommet";
+import { Box, Button, Select, Paragraph, RadioButtonGroup, Keyboard } from "grommet";
 import { LinkNext } from "grommet-icons";
+
+// Experiment configuration
+import { Configuration } from "src/configuration";
+
+// Keyboard input bindings
+import { BINDINGS } from "src/lib/bindings";
 
 /**
  * @summary Generate a 'Classification' screen containing a dropdown
@@ -24,8 +30,15 @@ const Classification: FC<Props.Screens.Classification> = (
   props: Props.Screens.Classification
 ): ReactElement => {
   // Configure relevant states
-  const [classification, setClassification] = React.useState("");
-  const [continueDisabled, setContinueDisabled] = React.useState(true);
+  const [classification, setClassification] = useState("");
+  const [continueDisabled, setContinueDisabled] = useState(true);
+
+  // Selected UI element (used for alternate input)
+  const [selectedElementIndex, setSelectedElementIndex] = useState(0);
+  const [elementFocused, setElementFocused] = useState(false);
+
+  // Classification selected index
+  const [classificationIndex, setClassificationIndex] = useState(0);
 
   const partners = [
     "To earn as much money for themselves as possible",
@@ -33,57 +46,120 @@ const Classification: FC<Props.Screens.Classification> = (
     "To share the money between us evenly",
   ];
 
-  return (
-    <Box
-      justify="center"
-      align="center"
-      gap="small"
-      animation={["fadeIn"]}
-      flex
-      direction="column"
-    >
-      {/* First question */}
-      <Paragraph margin="small" size="large" fill>
-        Overall, what do you think your partner was trying to do?
-      </Paragraph>
+  /**
+   * Handle keyboard input from user interaction
+   * @param {React.KeyboardEvent<HTMLElement>} event Keyboard input event
+   */
+  const inputHandler = (event: React.KeyboardEvent<HTMLElement>) => {
+    // Disable keyboard input if not enabled in configuration
+    if (Configuration.manipulations.useAlternateInput === false) return;
 
-      {/* Partner select component */}
-      <Box width="large">
-        <Select
-          a11yTitle="Select"
-          options={partners}
-          placeholder="Please select"
-          onChange={({ option }) => {
-            // Enable the continue button
-            setContinueDisabled(false);
+    // Avoid holding the key down if no element focused
+    if (event.repeat) return;
+    event.preventDefault();
 
-            // Update the selected classification
-            setClassification(option);
-          }}
-          margin={{ top: "large" }}
-          size="large"
-        />
-      </Box>
+    if (event.key.toString() === BINDINGS.NEXT || event.key.toString() === BINDINGS.PREVIOUS) {
+      if (elementFocused === true) {
+        if (selectedElementIndex === 0) {
+          let updatedIndex = classificationIndex;
+          // Radio button group
+          if (classification === "") {
+            updatedIndex = 0;
+          } else if (event.key.toString() === BINDINGS.PREVIOUS) {
+            updatedIndex = classificationIndex - 1 < 0 ? 0 : classificationIndex - 1;
+          } else if (event.key.toString() === BINDINGS.NEXT) {
+            updatedIndex = classificationIndex + 1 > partners.length - 1 ? classificationIndex : classificationIndex + 1;
+          }
 
-      {/* Continue button */}
-      <Button
-        a11yTitle="Continue"
-        primary
-        margin={{ top: "large" }}
-        color="button"
-        label="Continue"
-        disabled={
-          // Disabled until a partner type has been chosen
-          continueDisabled
+          // Enable the continue button
+          setContinueDisabled(false);
+
+          // Update state
+          setClassification(partners[updatedIndex]);
+          setClassificationIndex(updatedIndex);
         }
-        size="large"
-        icon={<LinkNext />}
-        reverse
-        onClick={() => {
+      } else {
+        if (event.key.toString() === BINDINGS.NEXT) {
+          setSelectedElementIndex(selectedElementIndex + 1 < 2 ? selectedElementIndex + 1 : 1);
+        } else if (event.key.toString() === BINDINGS.PREVIOUS) {
+          setSelectedElementIndex(selectedElementIndex - 1 >= 0 ? selectedElementIndex - 1 : 0);
+        }
+      }
+    } else if (event.key.toString() === BINDINGS.SELECT) {
+      if (selectedElementIndex !== 1) {
+        // Focus or unfocus sliders
+        setElementFocused(!elementFocused);
+      } else {
+        // Select the `Continue` button if permitted
+        if (!continueDisabled) {
           props.handler(classification);
-        }}
-      />
-    </Box>
+        }
+      }
+    }
+  };
+
+  return (
+    <Keyboard onKeyDown={inputHandler} target={"document"}>
+      <Box
+        justify="center"
+        align="center"
+        gap="medium"
+        animation={["fadeIn"]}
+        flex
+        direction="column"
+      >
+        {/* First question */}
+        <Paragraph margin="small" size="large" fill>
+          Overall, what do you think your partner was trying to do?
+        </Paragraph>
+
+        {/* Partner select component */}
+        <Box
+          border={selectedElementIndex === 0 && !elementFocused && { color: "selectedElement", size: "large" }}
+          pad={selectedElementIndex === 0 ? "xsmall" : "small"}
+          round
+        >
+          <RadioButtonGroup
+            name="partner-classification"
+            options={partners}
+            value={classification}
+            onChange={(event) => {
+              // Enable the continue button
+              setContinueDisabled(false);
+
+              // Update the selected classification
+              setClassification(event.target.value);
+            }}
+          />
+        </Box>
+
+        {/* Continue button */}
+        <Box
+          margin={"none"}
+          pad={"none"}
+          border={Configuration.manipulations.useAlternateInput === true && selectedElementIndex === 1 && { color: "selectedElement", size: "large" }}
+          style={Configuration.manipulations.useAlternateInput === true && selectedElementIndex === 1 ? { borderRadius: "36px "} : {}}
+          round
+        >
+          <Button
+            a11yTitle="Continue"
+            primary
+            color="button"
+            label="Continue"
+            disabled={
+              // Disabled until a partner type has been chosen
+              continueDisabled
+            }
+            size="large"
+            icon={<LinkNext />}
+            reverse
+            onClick={() => {
+              props.handler(classification);
+            }}
+          />
+        </Box>
+      </Box>
+    </Keyboard>
   );
 };
 
