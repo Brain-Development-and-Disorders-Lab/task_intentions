d172a17ed234b613029e3f75c7d783cb360b49c8
Henry Burgess <henryjburg@gmail.com>
2025-01-21 13:33:48 -0600

    INT-48 Copy packages during build

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,6 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = () => {
   return merge(common, {
@@ -7,5 +8,10 @@ module.exports = () => {
     mode: "production",
     target: ["web", "es5"],
     devtool: "inline-source-map",
+    plugins: [
+      new CopyPlugin({
+        patterns: [{ from: "src/data/packages", to: "packages" }],
+      }),
+    ],
   });
 };
