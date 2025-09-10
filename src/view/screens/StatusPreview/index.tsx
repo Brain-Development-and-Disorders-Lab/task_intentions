36d32ab7ed193fe46376c6ddafce8fb06d21d30f
Henry Burgess <henryjburg@gmail.com>
2025-09-10 14:06:16 -0500

    INT-74 Update `StatusPreview` screen

--- src/view/screens/StatusPreview/index.tsx
+++ src/view/screens/StatusPreview/index.tsx
@@ -1,12 +1,10 @@
 /**
- * @file `Loaded` screen to confirm completion of loading process.
+ * @file `StatusPreview` screen to show participants their partner's social standing.
  *
- * This screen displays the newly assigned partner avatar to participants
- * at the start of each game phase. Key features include:
+ * This screen displays the partner's social standing to participants
+ * at the start of a game phase. Key features include:
+ * - Partner social standing presentation
  * - Partner avatar presentation
- * - Unique partner assignment per phase
- * - State management for partner tracking
- * - Consistent partner identity across sessions
  *
  * @author Henry Burgess <henry.burgess@wustl.edu>
  */
@@ -15,7 +13,7 @@
 import React, { FC, ReactElement } from "react";
 
 // Grommet UI components
-import { Box, Button, Layer, Paragraph, WorldMap } from "grommet";
+import { Box, Button, Layer, Paragraph } from "grommet";
 import { LinkNext } from "grommet-icons";
 import Status from "src/view/components/Status";
 import Avatar from "boring-neutral-avatars";
@@ -24,12 +22,14 @@ import Avatar from "boring-neutral-avatars";
 import { Configuration } from "src/configuration";
 
 /**
- * @summary Generate a 'Loaded' screen containing a card with the partner avatar for the subsequent phase of the game
- * @param {FC} props Empty props object as this component takes no props
- * @return {ReactElement} 'Loaded' screen with partner avatar and success message
+ * @summary Generate a 'StatusPreview' screen containing a card with the partner avatar for the subsequent phase of the game
+ * @param {FC} props Props object containing:
+ *  - isPartnerHighStatus: {boolean} Whether the partner is in high status
+ *  - handler: {function} Function to handle the continue button click
+ * @return {ReactElement} 'StatusPreview' screen with partner avatar and success message
  */
 const Loaded: FC<Props.Screens.StatusPreview> = (props: Props.Screens.StatusPreview): ReactElement => {
-  // Get the current partner avatar
+  // Get the current partner avatar and social standing
   const experiment = window.Experiment;
   const partnerStatus = props.isPartnerHighStatus ? experiment.getState().get("partnerHighStatus") : experiment.getState().get("partnerLowStatus");
 
@@ -39,40 +39,37 @@ const Loaded: FC<Props.Screens.StatusPreview> = (props: Props.Screens.StatusPrev
   ]
 
   return (
-    <>
-      <WorldMap color="map" fill="horizontal" />
-      <Layer plain full>
-        <Box justify="center" align="center" gap="small" responsive fill>
-          <Paragraph margin="small" size="large" fill>
-            In this phase you will be able to see your partner&apos;s social standing.
-          </Paragraph>
-          <Avatar
-            size={120}
-            name={partnerAvatar}
-            variant={Configuration.avatars.variant as "beam"}
-            colors={Configuration.avatars.colours}
-          />
-          <Status
-            participantStatus={experiment.getState().get("participantDefaultStatus")}
-            partnerStatus={partnerStatus}
-          />
-          <Paragraph margin="small" size="large" fill>
-            Their ID has been hidden to preserve anonymity.
-          </Paragraph>
-          <Button
-            a11yTitle="Continue"
-            primary
-            color="button"
-            label="Continue"
-            icon={<LinkNext />}
-            reverse
-            onClick={() => {
-              props.handler();
-            }}
-          />
-        </Box>
-      </Layer>
-    </>
+    <Layer plain full>
+      <Box justify="center" align="center" gap="small" responsive fill>
+        <Paragraph margin="small" size="large" fill>
+          In this phase you will be able to see your partner&apos;s social standing.
+        </Paragraph>
+        <Avatar
+          size={100}
+          name={partnerAvatar}
+          variant={Configuration.avatars.variant as "beam"}
+          colors={Configuration.avatars.colours}
+        />
+        <Status
+          participantStatus={experiment.getState().get("participantDefaultStatus")}
+          partnerStatus={partnerStatus}
+        />
+        <Paragraph margin="small" size="large" fill>
+          Their ID has been hidden to preserve anonymity.
+        </Paragraph>
+        <Button
+          a11yTitle="Continue"
+          primary
+          color="button"
+          label="Continue"
+          icon={<LinkNext />}
+          reverse
+          onClick={() => {
+            props.handler();
+          }}
+        />
+      </Box>
+    </Layer>
   );
 };
 
